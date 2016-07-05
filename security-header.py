99d364a2b17bdd81038efe479d970703fe92a995
Allan Jay Dumanhug <atoooooooooom10@users.noreply.github.com>
2016-07-05 13:06:06 +0800

    Code Improvement
    List is better than ifs

--- security-header.py
+++ security-header.py
@@ -1,14 +1,14 @@
 import sys
 import urllib2
 
-url = raw_input('Enter the url: ')
+url = raw_input("Enter the url: ")
 
 if url.startswith("http://"):
     url
 elif url.startswith("https://"):
     url
 elif not url.startswith("http://") or not url.startswith("https://"):
-        url = "http://"+ url
+    url = "http://"+ url
 else:
     print ("Sorry! We can't recognize your input.")
     sys.exit()
@@ -17,68 +17,20 @@ request = urllib2.Request(url)
 response = urllib2.urlopen(request)
 securityHeaders = response.headers
 
+headers = ["Strict-Transport-Security","X-Frame-Options","X-XSS-Protection","X-Content-Type-Options","Content-Security-Policy",
+        "Cache-Control","X-Download-Options","X-Permitted-Cross-Domain-Policies","Public-Key-Pins","Public-Key-Pins-Report-Only"]
+
 print ("\nSecurity Headers:\n")
 
-if "Strict-Transport-Security" in securityHeaders:
-    print (" > Strict-Transport-Security")
+for header in headers:
+    if header in securityHeaders:
+        print (" > " + header)
 
-if "X-Frame-Options" in securityHeaders:
-    print (" > X-Frame-Options")
-
-if "X-XSS-Protection" in securityHeaders:
-    print (" > X-XSS-Protection")
-
-if "X-Content-Type-Options" in securityHeaders:
-    print (" > X-Content-Type-Options")
-
-if "Content-Security-Policy" in securityHeaders:
-    print (" > Content-Security-Policy")
-
-if "Cache-Control" in securityHeaders:
-    print (" > Cache-Control")
-
-if "X-Download-Options" in securityHeaders:
-    print (" > X-Download-Options")
-
-if "X-Permitted-Cross-Domain-Policies" in securityHeaders:
-    print (" > X-Permitted-Cross-Domain-Policies")
-
-if "Public-Key-Pins" in securityHeaders:
-    print (" > Public-Key-Pins")
-
-if "Public-Key-Pins-Report-Only" in securityHeaders:
-    print (" > Public-Key-Pins-Report-Only")
 
 print ("\nMissing Headers:\n")
 
-if "Strict-Transport-Security" not in securityHeaders:
-    print (" > Strict-Transport-Security")
-
-if "X-Frame-Options" not in securityHeaders:
-    print (" > X-Frame-Options")
-
-if "X-XSS-Protection" not in securityHeaders:
-    print (" > X-XSS-Protection")
-
-if "X-Content-Type-Options" not in securityHeaders:
-    print (" > X-Content-Type-Options")
-
-if "Content-Security-Policy" not in securityHeaders:
-    print (" > Content-Security-Policy")
-
-if "Cache-Control" not in securityHeaders:
-    print (" > Cache-Control")
-
-if "X-Download-Options" not in securityHeaders:
-    print (" > X-Download-Options")
-
-if "X-Permitted-Cross-Domain-Policies" not in securityHeaders:
-    print (" > X-Permitted-Cross-Domain-Policies")
-
-if "Public-Key-Pins" not in securityHeaders:
-    print (" > Public-Key-Pins")
-
-if "Public-Key-Pins-Report-Only" not in securityHeaders:
-    print (" > Public-Key-Pins-Report-Only")
+for header in headers:
+    if header not in securityHeaders:
+        print (" > " + header)
 
 response.close()
