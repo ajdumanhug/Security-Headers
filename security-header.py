577cdff28c6062e7ab6f95ba5166fb9713b70ff8
Julio C. Stefanutto <jcesarstef@gmail.com>
2018-03-03 18:05:33 -0300

    Added test for max-age and argparse
    Hello, i take the liberty to update your great script.
    I added the follow features:
     - Check for Strict-Transporty max age - It's a great practice use more than 10368000 seconds
     - argparser - add the option to use --url or -u to improve execution time
    
    Criticisms or suggestions are welcome

--- security-header.py
+++ security-header.py
@@ -1,7 +1,17 @@
 import sys
 import urllib2
+import re
+import argparse
 
-url = raw_input("Enter the url: ")
+parser = argparse.ArgumentParser(description = "It is a python tool developed to enumerate security headers of a website.")
+parser.add_argument("--url", "-u", action = "store", dest = "url", required = False, help = "Enter the url")
+arguments = parser.parse_args()
+
+
+if arguments.url:
+	url = arguments.url
+else:
+	url = raw_input("Enter the url: ")
 
 if url.startswith("http://"):
     url
@@ -20,11 +30,16 @@ securityHeaders = response.headers
 headers = ["Strict-Transport-Security","X-Frame-Options","X-XSS-Protection","X-Content-Type-Options","Content-Security-Policy",
         "Cache-Control","X-Download-Options","X-Permitted-Cross-Domain-Policies","Public-Key-Pins","Public-Key-Pins-Report-Only"]
 
-print ("\nSecurity Headers:\n")
+print ("\nSecurity Headers Found:\n")
 
 for header in headers:
     if header in securityHeaders:
-        print (" > " + header)
+	if (header == "Strict-Transport-Security"):
+		max_age = re.findall("max-age=(\w*)", securityHeaders["strict-transport-security"])[0]
+		if (int(max_age) < 10368000):
+			print ("\033[91m > " + header + ": " + securityHeaders[header] + "\033[0m\n    + Max-age is lower than 10368000 (seconds)\n    (https://blog.qualys.com/securitylabs/2016/03/28/the-importance-of-a-proper-http-strict-transport-security-implementation-on-your-web-server)\n")
+        else:
+		    print (" > " + header)
 
 
 print ("\nMissing Headers:\n")
